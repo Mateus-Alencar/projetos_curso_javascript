73a7c82bf26521a8cb5abb7c0c13201debd698e8
Mateus-Alencar <111691429+Mateus-Alencar@users.noreply.github.com>
2022-09-30 22:37:43 -0300

    primeiro commit

---
+++ arquivo.js
@@ -0,0 +1,4 @@
+// const = o valor da  variável não pode ser alterado durante o código
+// let pode sofre alterações dos seus valores durante a execução do código
+const idade = 5
+let nome = "Mateus"
